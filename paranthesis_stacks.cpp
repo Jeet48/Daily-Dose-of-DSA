d6022418238dbc91c48d85f4bdd52de44bdc79c7
Jeet Mondal <97789227+SaltyLasagna@users.noreply.github.com>
2022-04-04 11:59:37 +0530

    Check for Balanced Paranthesis

---
+++ paranthesis_stacks.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool areBracketBalanced(string expr)
+{
+	stack<char> s;
+	char x;
+
+	for(int i=0;i<expr.length(); i++)
+	{
+		if(expr[i] == '(' || expr[i] == '[' || expr[i] == '{' )
+		{
+			s.push(expr[i]);
+			continue;
+		}
+
+		if(s.empty())
+			return false;
+
+		switch(expr[i]) {
+
+			case ')':
+                     x=s.top();
+                     s.pop();
+                     if(x=='{'||x=='[')
+                     	return false;
+                     break;
+
+            case '}':
+                     x=s.top();
+                     s.pop();
+                     if(x=='('||x=='[')
+                     	return false;
+                     break;
+
+            case ']':
+                     x=s.top();
+                     s.pop();
+                     if(x=='('||x=='{')
+                     	return false;
+                     break;
+
+		}
+	}
+
+	return (s.empty());
+}
+
+int main()
+{
+	string expr;
+	cout<<"Enter the string required";
+	cin>>expr;
+
+	if(areBracketBalanced(expr))
+		cout<<"Balanced";
+	else
+		cout<<"Not Balanced";
+	return 0;
+}
